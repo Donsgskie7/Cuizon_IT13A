485b29de8f446781e90006e875f9ddc05718dbbf
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-04-04 17:01:34 +0800

    Added Fibonacci Code

---
+++ src/Midterms/IT13A_Cuizon_Fibonacci.java
@@ -0,0 +1,21 @@
+
+package midterms;
+
+public class IT13A_Cuizon_Fibonacci {
+     public static void main(String[] args){
+        int x = 10;
+       
+            for (int i = 0; i < x; i++) {
+            System.out.print(fibonacci(i) + " , ");
+    }
+}
+        public static int fibonacci(int x) {
+        if (x <= 1){
+          return x;
+       } else {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+                 }
+               }
+            }
+
+
